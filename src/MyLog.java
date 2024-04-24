96826198ae2f8cb7fc9aa4a321854b1f0e0ddf98
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2024-04-24 11:21:43 -0300

    feat: Add MyLog Singleton Class

---
+++ src/MyLog.java
@@ -0,0 +1,45 @@
+public class MyLog {
+
+private static MyLog uniqueInstance;
+    private LogLevel priorityLevel;
+
+    private enum LogLevel {
+        INFO,
+        WARNING,
+        ERROR
+    }
+private void Mylog(){
+    LogLevel priorityLevel;
+}
+
+    private MyLog() {
+        this.priorityLevel = LogLevel.INFO;
+    }
+
+    public static synchronized MyLog getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new MyLog();
+        }
+        return uniqueInstance;
+    }
+    public void setPriority(LogLevel level) {
+        this.priorityLevel = level;
+    }
+
+    public void log(String message, LogLevel level) {
+        if (level.ordinal() >= this.priorityLevel.ordinal()) {
+            switch (level) {
+                case INFO:
+                    System.out.println("Information message: " + message);
+                    break;
+                case WARNING:
+                    System.out.println("Warning message: " + message);
+                    break;
+                case ERROR:
+                    System.out.println("Error message: " + message);
+                    break;
+            }
+        }
+    }
+}
+
